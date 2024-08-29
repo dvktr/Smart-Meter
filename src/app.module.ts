7372f21c491b9c85f122d1035787c208f1693354
Victor Mendes <victormendes04012004@gmail.com>
2024-08-29 15:29:32 -0300

    dev(SM-02): use TypeOrm configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [],
   providers: [],
 })
