b1c71344af387cccdd16040ac0ba4811bcf3c546
Victor Mendes <victormendes04012004@gmail.com>
2024-08-29 15:49:03 -0300

    dev(SM-02): import customer and measure resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
+import { CustomerModule } from './resources/customer/customer.module';
+import { MeasureModule } from './resources/measure/measure.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    CustomerModule,
+    MeasureModule,
   ],
   controllers: [],
   providers: [],
