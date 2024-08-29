1417a38471adcad45ed39cd80847823945198e85
Victor Mendes <victormendes04012004@gmail.com>
2024-08-29 15:28:25 -0300

    dev(SM-02): create_table_customer, create_table_measure and relate_table_customer_measure migrations

---
+++ src/migrations/1724954454402-create_table_measure.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableMeasure1724954454402 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        CREATE TABLE measure (
+            id serial  NOT NULL,
+            type varchar(255)  NOT NULL,
+            value decimal(10,2)  NOT NULL,
+            image_url int  NOT NULL,
+            confirmed boolean  NOT NULL DEFAULT false,
+            customer_id int  NOT NULL,
+            CONSTRAINT measure_pk PRIMARY KEY (id)
+        );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        DROP TABLE measure    
+    `);
+  }
+}
