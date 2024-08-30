e4439eb0ae9ad5358b5f68f6f59da4a8661693f7
Victor Mendes <victormendes04012004@gmail.com>
2024-08-30 16:43:26 -0300

    dev(SM-03): adjust app to use validationPipes and global filters

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './exceptions.filter';
+import { urlencoded, json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT);
 }
 bootstrap();
