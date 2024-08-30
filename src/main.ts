1de487f34a35d8a9a8f2b895e706ccb44b54d03f
Victor Mendes <victormendes04012004@gmail.com>
2024-08-30 17:37:50 -0300

    dev(SM-03): remove prefix /api

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { urlencoded, json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
