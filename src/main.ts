79705325f9bcae7a761ca6d1f920d1a9a3465325
Victor Mendes <victormendes04012004@gmail.com>
2024-08-30 23:10:23 -0300

    dev(SM-06): use swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './exceptions.filter';
 import { urlencoded, json } from 'body-parser';
+import { SwaggerModule } from '@nestjs/swagger';
+import { configSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,10 @@ async function bootstrap() {
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
